a5d11f82bce2faa0353047829175af594da8b823
Islam-19984 <i.hemada@alustudent.com>
2023-07-20 13:55:47 -0700

    100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
 # disables authentication
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh_config':
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
         path    => '/bin/'
 }
